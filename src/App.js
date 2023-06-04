231eb71891c2e3924490196627c3a042f63b02fe
serhatprogramming <serhatprogramming@gmail.com>
2023-06-04 13:15:56 -0400

    init css component states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import republican from "./republican.png";
+import democratic from "./democratic.png";
+import american from "./american.png";
 
-function App() {
+import { useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  const [democrats, setDemocrats] = useState(0);
+  const [republicans, setRepublicans] = useState(0);
+  const [independent, setIndependent] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="party-container">
+      <div className="party">
+        <h2>Democrats</h2>
+        <div className="box blue">
+          <img src={democratic} alt="Democrats" className="party-image" />
+          <br />
+          <button
+            onClick={() => setDemocrats(democrats + 1)}
+            className="vote-button"
+          >
+            Vote
+          </button>
+          <p className="vote-count">Vote Count: {democrats}</p>
+        </div>
+      </div>
+
+      <div className="party">
+        <h2>Republicans</h2>
+        <div className="box red">
+          <img src={republican} alt="Republicans" className="party-image" />
+          <br />
+          <button
+            onClick={() => setRepublicans(republicans + 1)}
+            className="vote-button"
+          >
+            Vote
+          </button>
+          <p className="vote-count">Vote Count: {republicans}</p>
+        </div>
+      </div>
+
+      <div className="party">
+        <h2>Independent</h2>
+        <div className="box yellow">
+          <img src={american} alt="Independents" className="party-image" />
+          <br />
+          <button
+            onClick={() => setIndependent(independent + 1)}
+            className="vote-button"
+          >
+            Vote
+          </button>
+          <p className="vote-count">Vote Count: {independent}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
