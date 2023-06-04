2e3fa8b5b05a14c70eb25405ac70ca96548db254
serhatprogramming <serhatprogramming@gmail.com>
2023-06-04 13:58:56 -0400

    indiv comps

--- src/App.js
+++ src/App.js
@@ -6,11 +6,38 @@ import { useState } from "react";
 
 import "./App.css";
 
-const App = () => {
-  const [democrats, setDemocrats] = useState(0);
-  const [republicans, setRepublicans] = useState(0);
-  const [independent, setIndependent] = useState(0);
+const ElectionResults = ({ democrats, republicans, independent }) => {
+  const totalVotes = democrats + republicans + independent;
 
+  const democratPercentage = (democrats / totalVotes) * 100 || 0;
+  const republicanPercentage = (republicans / totalVotes) * 100 || 0;
+  const independentPercentage = (independent / totalVotes) * 100 || 0;
+
+  return (
+    <div className="vote-statistics">
+      <h3>Vote Statistics</h3>
+      <p>Total Votes: {totalVotes}</p>
+      <p>
+        Democrats: {democrats} ({democratPercentage.toFixed(2)}%)
+      </p>
+      <p>
+        Republicans: {republicans} ({republicanPercentage.toFixed(2)}%)
+      </p>
+      <p>
+        Independent: {independent} ({independentPercentage.toFixed(2)}%)
+      </p>
+    </div>
+  );
+};
+
+const Ballot = ({
+  democrats,
+  republicans,
+  independent,
+  setDemocrats,
+  setRepublicans,
+  setIndependent,
+}) => {
   return (
     <div className="party-container">
       <div className="party">
@@ -58,6 +85,30 @@ const App = () => {
         </div>
       </div>
     </div>
+  );
+};
+
+const App = () => {
+  const [democrats, setDemocrats] = useState(0);
+  const [republicans, setRepublicans] = useState(0);
+  const [independent, setIndependent] = useState(0);
+
+  return (
+    <>
+      <Ballot
+        democrats={democrats}
+        republicans={republicans}
+        independent={independent}
+        setDemocrats={setDemocrats}
+        setRepublicans={setRepublicans}
+        setIndependent={setIndependent}
+      />
+      <ElectionResults
+        democrats={democrats}
+        republicans={republicans}
+        independent={independent}
+      />
+    </>
   );
 };
 
